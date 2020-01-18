dfbc53e1b7c1c7fc2f297ed8ef81f1dca538423d
NickVeliki <nickveliki@gmail.com>
2020-01-18 19:27:16 +0100

    fs in index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http2 = require("http2");
 const app = require("./app/app");
+const fs = require("fs");
 http2.createServer({
     key: fs.readFileSync("/etc/letsencrypt/live/thern.wtf/privkey.pem"),
     cert: fs.readFileSync("/etc/letsencrypt/live/thern.wtf/fullchain.pem")
