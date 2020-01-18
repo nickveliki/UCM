405b8c9e730271a95bc492ccdca4626b8ac72708
NickVeliki <nickveliki@gmail.com>
2020-01-18 20:26:01 +0100

    server fix

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const https = require("https");
 const app = require("./app/app");
 const fs = require("fs");
-https.createServer({
+const server = https.createServer({
     key: fs.readFileSync("/etc/letsencrypt/live/thern.wtf/privkey.pem"),
     cert: fs.readFileSync("/etc/letsencrypt/live/thern.wtf/fullchain.pem")
-},app).listen(process.argv[2]||443);
+},app)
+server.listen(process.argv[2]||443);
