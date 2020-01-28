3a0f6f2a59b2fe26e641c2c31f58c09e3c6708d6
NickVeliki <nickveliki@gmail.com>
2020-01-28 21:49:01 +0100

    Krrrrghhhhh

--- htdocs/service-worker.js
+++ htdocs/service-worker.js
@@ -2,8 +2,13 @@ importScripts("precache-manifest.a752689030148ab929b671fbc87f0cec.js", "https://
 
 workbox.core.skipWaiting();
 self.addEventListener('activate', event => {
-    clients.claim();
-    console.log('Ready!');
+    clients.claim().then((clients)=>{
+      if(clients.length>0){
+        clients[0].postMessage(JSON.stringify({title:"LET'S ROCK!", body:"You are now using notifications", show:true}))
+      }else{
+        registration.showNotification("No Clients Available!")
+      }
+    });
 });
 let client;
 workbox.routing.registerRoute(
@@ -15,13 +20,14 @@ workbox.routing.registerRoute(
     source.postMessage(JSON.stringify({show: true, title:"READY TO ROCK!", body:"You can now receive notifications on this app. Please allow them, we promise we won't bother you!"}))
 })*/
 self.addEventListener('push', ({data}) => {
-  self.clients.matchAll().then((clients)=>{
+  
+  clients.matchAll().then((clients)=>{
     if(clients.length>0){
       console.log("found a client");
       clients[0].postMessage(data.text());
     }else{
       console.log("claiming clients");
-      self.clients.claim().then(()=>{
+      clients.claim().then(()=>{
         clients[0].postMessage(data.text());
       })
     }
